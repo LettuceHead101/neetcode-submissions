cbe9bea2cb4008407889564eda76013c89662532
LettuceHead101 <vegito026@gmail.com>
2026-05-26 13:21:15 -0500

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hashmap;
+
+        for(string s : strs) {
+            vector<char> freqCount(26, 0);
+            for(char c : s) {
+                freqCount[c - 'a']++;
+            }
+            
+            string serializedString = "";
+            for(int i = 0; i < 26; i++) {
+                serializedString += freqCount[i];
+            }
+
+            hashmap[serializedString].push_back(s);
+        }
+        vector<vector<string>> result;
+
+        for(auto p : hashmap) {
+            result.push_back(p.second);
+        }
+
+        return result;
+    }
+};
