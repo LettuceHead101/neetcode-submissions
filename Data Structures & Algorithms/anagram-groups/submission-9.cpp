80cd1635455bdbd37536108cd54b1e2def075a43
LettuceHead101 <vegito026@gmail.com>
2026-06-10 22:54:57 -0500

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+
+        for(string s : strs) {
+            vector<int> freq(26, 0);
+            for(char c : s) {
+                freq[c - 'a']++;
+            }
+            string temp = "";
+
+            for(int i = 0; i < 26; i++) {
+                temp += freq[i];
+            }
+
+            m[temp].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for(auto it : m) {
+            result.push_back(it.second);
+        }
+
+        return result;
+    }
+};
