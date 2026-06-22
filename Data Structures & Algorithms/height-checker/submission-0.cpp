0018c9f3c2967e72457f575e192d8d5f470faa9a
LettuceHead101 <vegito026@gmail.com>
2026-06-22 13:08:46 -0500

    Add: height-checker - submission-0

---
+++ Data Structures & Algorithms/height-checker/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sorted(heights.begin(), heights.end());
+        sort(sorted.begin(), sorted.end());
+        int count = 0;
+        for(int i = 0; i < heights.size(); i++) {
+            if(heights[i] != sorted[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
