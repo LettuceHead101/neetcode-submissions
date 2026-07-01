cd721ccf48a9d331866cdf6de49d9b040961db11
LettuceHead101 <vegito026@gmail.com>
2026-07-01 15:26:45 -0500

    Add: isomorphic-strings - submission-5

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+
+        unordered_map<char, char> m;
+        unordered_set<char> mapped_t;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (m.find(s[i]) != m.end()) {
+                if (m[s[i]] != t[i]) {
+                    return false;
+                }
+            } else {
+                if (mapped_t.find(t[i]) != mapped_t.end()) {
+                    return false; 
+                }
+                
+                m[s[i]] = t[i];
+                mapped_t.insert(t[i]);
+            }
+        }
+
+        return true;
+    }
+};
