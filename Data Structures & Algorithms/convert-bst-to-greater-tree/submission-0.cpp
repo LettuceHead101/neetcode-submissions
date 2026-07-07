f9011955c9dbcfd95e47d24900a3cb1e4174bbb0
LettuceHead101 <vegito026@gmail.com>
2026-07-07 17:10:02 -0500

    Add: convert-bst-to-greater-tree - submission-0

---
+++ Data Structures & Algorithms/convert-bst-to-greater-tree/submission-0.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int totalSum;
+
+    TreeNode* convertBST(TreeNode* root) {
+        totalSum = getSum(root);
+        dfs(root);
+        return root;
+    }
+
+    int getSum(TreeNode* node){
+        if(!node) return 0;
+        return node->val + getSum(node->left) + getSum(node->right);
+    }
+
+    void dfs(TreeNode* node){
+        if(!node) return;
+
+        dfs(node->left);
+        int v = node->val;
+        node->val = totalSum;
+        totalSum -= v;
+        dfs(node->right);
+    }
+};
