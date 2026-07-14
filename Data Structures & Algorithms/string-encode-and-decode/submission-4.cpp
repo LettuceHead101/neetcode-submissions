1c8f664d130dc560c75f8a7592f0130c921f66b6
LettuceHead101 <vegito026@gmail.com>
2026-07-14 14:34:27 -0500

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans = "";
+
+        for(const auto& word : strs){
+            int size = word.size();
+
+            ans += to_string(size);
+            ans += "#";
+            ans += word;
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+
+        int i = 0; 
+        while(i < s.size()){
+            int j = i;
+            while(s[j] != '#'){
+                j++;
+            }
+            int length = stoi(s.substr(i, j - i));
+            i = j + 1;
+            j = i + length;
+            result.push_back(s.substr(i, length));
+            i = j;
+        }
+
+        return result;
+    }
+};
