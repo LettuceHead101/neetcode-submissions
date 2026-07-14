c1b372c780d2b89ced5df9c013399b03c79827cd
LettuceHead101 <vegito026@gmail.com>
2026-07-14 13:24:11 -0500

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<int, int> seen;
+        for(int i = 0; i < s.size(); i++){
+            seen[s[i]]++;
+            seen[t[i]]--;
+        }
+
+        for(auto& [_,v] : seen){
+            if(v != 0) return false;
+        }
+        return true;
+    }
+};
